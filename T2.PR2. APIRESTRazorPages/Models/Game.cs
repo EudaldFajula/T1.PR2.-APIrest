999b931aa84fec85dac1033de94d757fbf01cf1c
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-04-22 04:18:08 +0200

    feat(client): Afegir models i DTOs

---
+++ T2.PR2. APIRESTRazorPages/Models/Game.cs
@@ -0,0 +1,12 @@
+﻿namespace T2.PR2._APIRESTRazorPages.Models
+{
+	public class Game
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string DeveloperTeam { get; set; }
+		public string ImgUrl { get; set; }
+		public IList<string> VotingUsers { get; set; } = new List<string>();
+	}
+}
