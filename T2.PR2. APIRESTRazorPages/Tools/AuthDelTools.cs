8112b43b44cdd9afbd72e591464d1bf746616967
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-04-22 05:22:14 +0200

    feat(client): Afegir metodes que conecten a la API

---
+++ T2.PR2. APIRESTRazorPages/Tools/AuthDelTools.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+
+namespace T2.PR2._APIRESTRazorPages.Tools
+{
+    public class AuthDelTools : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuthDelTools(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = _httpContextAccessor.HttpContext.Request.Cookies["authToken"];
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    
+    }
+}
---
+++ T2.PR2. APIRESTRazorPages/Tools/AuthTools.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using T2.PR2._APIRESTRazorPages.Models;
+using System.Text.Json;
+
+namespace T2.PR2._APIRESTRazorPages.Tools
+{
+    public class AuthTools
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuthTools(HttpClient httpClient, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClient = httpClient;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<(bool Success, string Message)> Login(LoginUser loginModel)
+        {
+            loginModel.Password = Convert.ToBase64String(Encoding.UTF8.GetBytes(loginModel.Password));
+
+            var content = new StringContent(
+                JsonSerializer.Serialize(loginModel),
+                Encoding.UTF8,
+                "application/json");
+            var response = await _httpClient.PostAsync("/api/Auth/login", content);
+            var body = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                
+                return (false, body);
+            }
+
+            var token = body.Trim();
+
+            var handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jwt;
+            try
+            {
+                jwt = handler.ReadJwtToken(token);
+            }
+            catch
+            {
+                return (false, "Invalid token format");
+            }
+
+            var identity = new ClaimsIdentity(
+                jwt.Claims,
+                CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+
+            await _httpContextAccessor.HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                principal,
+                new AuthenticationProperties
+                {
+                    IsPersistent = loginModel.RememberMe,
+                    ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(30),
+                    AllowRefresh = true
+                });
+
+            _httpContextAccessor.HttpContext.Response.Cookies.Append(
+               "authToken",
+               token,
+               new CookieOptions
+               {
+                   HttpOnly = true,
+                   Secure = true,
+                   SameSite = SameSiteMode.Strict,
+                   Expires = loginModel.RememberMe
+                       ? DateTimeOffset.UtcNow.AddDays(7)
+                       : DateTimeOffset.UtcNow.AddHours(1)
+               });
+            return (true, "");
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _httpContextAccessor.HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var apiTokenCookieOptions = new CookieOptions
+            {
+                Expires = DateTimeOffset.Now.AddDays(-1),
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+            };
+
+            _httpContextAccessor.HttpContext.Response.Cookies.Delete("ApiToken", apiTokenCookieOptions);
+        }
+    }
+}
