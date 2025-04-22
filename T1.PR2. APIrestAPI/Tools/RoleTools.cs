43095365294d4a2b1f5b19c7dac39af2daf72cc3
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2025-04-22 03:58:44 +0200

    feat(api): Afegir els rols.

---
+++ T1.PR2. APIrestAPI/Tools/RoleTools.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace T1.PR2._APIrestAPI.Tools
+{
+    public static class RoleTools
+    {
+        public static async Task CreateInitialsRoles(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string[] rols = { "Admin", "User" };
+
+            foreach (var rol in rols)
+            {
+                if (!await roleManager.RoleExistsAsync(rol))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(rol));
+                }
+            }
+        }
+    }
+}
